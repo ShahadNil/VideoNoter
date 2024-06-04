14fe82b2a2d1744e040f733d309c9ebfbe948ddd
Shakirul Hasan Shahad <shakirulhasanpersonal@gmail.com>
2024-06-04 22:25:56 +0600

    updated

--- keynoter.py
+++ keynoter.py
@@ -51,8 +51,7 @@ prompts = [
       6. Write every detail you need to satisfy the task. You can write upto 10000 words , so do not hesitate using more words.. 
       7. Act as a professional , do not include casual words.
       8. Take informations from visuals and audio when the visual is related the main topic. If the visuals are not related to the main lecture, ignore it.
-      9. Names used in the whole note should be italic (*NAME*).
-      10. You can provide links (if there) and if it is important to provide the link using markdown [LINK_NAME](LINK)
+      9. You can provide links (if there) and if it is important to provide the link using markdown [LINK_NAME](LINK)
 
       Key Responsibilites:
       1. Provide a detailed summary of the whole lecture at first.
