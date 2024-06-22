f94a57c48ce1a3feeaae1a038f0970cd0463ed99
Shakirul Hasan Shahad <shakirulhasanpersonal@gmail.com>
2024-06-22 23:03:26 +0600

    Updated

--- keynoter.py
+++ keynoter.py
@@ -140,7 +140,7 @@ prompts = [
 ]
 
 
-model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings,
                               system_instruction=prompts[0])
